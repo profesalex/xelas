fa8fd334d2a354b2e7c15590df39e02486909ab9
profesalex <profesalex@mail.ru>
2016-08-24 13:52:50 +0300

    change index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Xelas</title>
 </head>
 <body>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quaerat debitis, quam distinctio laborum animi dolore adipisci dignissimos cupiditate, id sit voluptatem optio repudiandae numquam. Deleniti expedita iusto quod! Illum?</p>
+	<p>Lsdsorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quaerat debitis, quam distinctio laborum animi dolore adipisci dignissimos cupiditate, id sit voluptatem optio repudiandae numquam. Deleniti expedita iusto quod! Illum?</p>
 	<p>Placeat similique ut velit ullam porro ex doloremque laudantium harum sit! Molestiae recusandae, nisi consequuntur. Obcaecati labore corporis perferendis molestias at atque autem cupiditate minus sed eum dolores, molestiae repudiandae.</p>
 	<p>Vela numquam illo laudantium id at eum sunt aliquid necessitatibus architecto doloribus voluptas tenetur, voluptatem, reiciendis possimus. Totam voluptates, soluta consectetur possimus doloremque. Obcaecati praesentium nesciunt, suscipit veniam mollitia at.</p>
 	<p>Fugit voluptates, voluptas eaque? Tempora assumenda porro, ducimus in tempore, aliquid eos aspernatur, aliquam odit iste voluptatem repellat aut alias. Cupiditate commodi provident similique qui deserunt nihil quam sit temporibus.</p>
