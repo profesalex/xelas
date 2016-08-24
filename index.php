5b051a93e3b1facc3c429bfb46e2d58bd939bf36
profesalex <profesalex@mail.ru>
2016-08-24 11:22:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Xelas</title>
+</head>
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quaerat debitis, quam distinctio laborum animi dolore adipisci dignissimos cupiditate, id sit voluptatem optio repudiandae numquam. Deleniti expedita iusto quod! Illum?</p>
+	<p>Placeat similique ut velit ullam porro ex doloremque laudantium harum sit! Molestiae recusandae, nisi consequuntur. Obcaecati labore corporis perferendis molestias at atque autem cupiditate minus sed eum dolores, molestiae repudiandae.</p>
+	<p>Vel numquam illo laudantium id at eum sunt aliquid necessitatibus architecto doloribus voluptas tenetur, voluptatem, reiciendis possimus. Totam voluptates, soluta consectetur possimus doloremque. Obcaecati praesentium nesciunt, suscipit veniam mollitia at.</p>
+	<p>Fugit voluptates, voluptas eaque? Tempora assumenda porro, ducimus in tempore, aliquid eos aspernatur, aliquam odit iste voluptatem repellat aut alias. Cupiditate commodi provident similique qui deserunt nihil quam sit temporibus.</p>
+	<p>Explicabo cupiditate, reiciendis cum excepturi similique commodi, vero, illo nostrum dolores nemo accusantium veniam. Omnis ea ipsam quisquam est dignissimos blanditiis cum voluptatibus harum porro nulla neque enim, repellat impedit.</p>
+</body>
+</html>
